041cc65134ab24ca5f04e80ff258e28f1648ad0f
espiritocz <espiritocz@gmail.com>
2023-06-13 17:38:57 +0100

    Update setFrameInactive.py
    ensure deactivation works as expected

--- python/setFrameInactive.py
+++ python/setFrameInactive.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
-from batchDBLib import get_polyid,set_inactive
+from batchDBLib import get_polyid,set_inactive,get_user
 import sys
-
+import os
 frame=sys.argv[1]
-polyID = get_polyid(sys.argv[1])
-print('Deactivating frame '+frame+' and removing related fields from framebatch database')
-set_inactive(polyID)
+userid = get_user(frame)
+
+if not userid:
+    print('frame is already inactive')
+    exit()
+
+
+if userid == os.environ['USER']:
+    print('Deactivating frame '+frame+' and removing related fields from framebatch database')
+    polyID = get_polyid(frame)
+    set_inactive(polyID)
+else:
+    print('this frame is active under different user: '+userid+'. Cancelling deactivation.')
