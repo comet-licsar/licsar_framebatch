98f56e5a8bd9c2c8ff6dc73e464417717e38a08a
espiritocz <espiritocz@gmail.com>
2024-10-29 22:11:03 +0000

    minor edit

--- bin/framebatch_alive.sh
+++ bin/framebatch_alive.sh
@@ -11,16 +11,17 @@ echo "there are "$totalno" frames to update"
 if [ -f $inputfile.lock ]; then echo "Update is locked - did the previous processing not finish??? Check it"; rm $inputfile.lock; exit; fi
 touch $inputfile.lock
 
+if [ $onlyPOD == 1 ]; then
+  enddate=`date -d '21 days ago' +%Y-%m-%d`
+ else
+  enddate=`date +%Y-%m-%d`
+fi
+
 i=0
 for frame in `cat $inputfile`; do
  let i=$i+1
  echo "Processing frame no. "$i"/"$totalno" (frame "$frame"):"
  date
- if [ $onlyPOD == 1 ]; then
-  enddate=`date -d '21 days ago' +%Y-%m-%d`
- else
-  enddate=`date +%Y-%m-%d`
- fi
 
  #debug - delete gapfilling and temp folders
  rm -rf $LiCSAR_temp/$frame 2>/dev/null
@@ -32,9 +33,9 @@ for frame in `cat $inputfile`; do
   sleep 400
  else
   tr=`echo $frame | cut -d '_' -f1 | cut -c -3 | sed 's/^0//' | sed 's/^0//'`
-  master=`ls $LiCSAR_procdir/$tr/$frame/SLC | head -n1`
+  # master=`ls $LiCSAR_procdir/$tr/$frame/SLC | head -n1`
   #get last epoch based on public interferograms
-  lastepoch=`ls $LiCSAR_public/$tr/$frame/interferograms/2*_2* -d | tail -n1 | rev | cut -d '_' -f1 | rev`
+  lastepoch=`ls $LiCSAR_public/$tr/$frame/interferograms | grep ^2 | tail -n1 | rev | cut -d '_' -f1 | rev`
   # start date will include at least last three images to process..
   startdate=`date -d $lastepoch'-37 days' +%Y-%m-%d`
   licsar_make_frame.sh -S -N $forcee -P $frame 0 1 $startdate $enddate #>$BATCH_CACHE_DIR/volc/auto_volc_$frame.log 2>$BATCH_CACHE_DIR/volc/auto_volc_$frame.err
