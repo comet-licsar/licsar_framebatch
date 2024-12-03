1a65102610b1145536628de286e10f2abb1a3440
espiritocz <espiritocz@gmail.com>
2024-12-03 22:22:38 +0000

    volq_process to run on subsets

--- bin/volq_process.sh
+++ bin/volq_process.sh
@@ -11,6 +11,7 @@ if [ -z $1 ]; then
  echo "parameter -l means to run from lowres (additionally, with parameter -p it will clip to the extents as on volcano portal that is 55 km diameter)"
  echo "parameter -M X means target multilook factor (only for hires regime - by default -M 3)"
  echo "parameter -C 0.X would apply additional masking based on individual coherence"
+ echo "-s would use sid"
  #echo "----"
  echo "this will copy and process ifgs and store in \$BATCH_CACHE_DIR/subsets/\$sid/\$frameid directory"
  echo "---"
@@ -26,14 +27,17 @@ lowres=0
 ml=3
 clipasportal=0
 volcid=''
+sid=''
 
-while getopts ":PRlpLn:i:C:M:v:" option; do
+while getopts ":PRlpLs:n:i:C:M:v:" option; do
  case "${option}" in
   P) extra='-P ';
      ;;
   i ) vid=$OPTARG;
      ;;
   M ) ml=$OPTARG;
+     ;;
+  s ) sid=$OPTARG;
      ;;
   L ) regime='licsar';
      ;;
@@ -64,15 +68,30 @@ fi
 if [ ! -z $vid ]; then
  echo "Running processing for volclip ID "$vid
 else
- echo "error finding volclip ID, cancelling"
- exit
+  if [ -z $sid ]; then
+  echo "error finding volclip ID, cancelling"
+  exit
+ fi
 fi
 #if [ -z $1 ]; then echo "please check provided parameters"; exit; fi
 
 if [ $regime == 'licsar' ]; then
-  tempfile=$BATCH_CACHE_DIR/$vid.frames
-  python3 -c "import volcdb; vid="$vid"; volcid=volcdb.get_volcano_from_vid(vid); print(volcdb.get_volcano_frames(volcid))"> $tempfile
-  for x in `grep '\[' $tempfile | sed "s/'//g" | sed 's/\[//' | sed 's/\]//' | sed 's/\,//'`; do
+  if [ -z $sid ]; then
+   tempfile=$BATCH_CACHE_DIR/$vid.frames
+   python3 -c "import volcdb; vid="$vid"; volcid=volcdb.get_volcano_from_vid(vid); print(volcdb.get_volcano_frames(volcid))"> $tempfile.bb
+   for x in `grep '\[' $tempfile.bb | sed "s/'//g" | sed 's/\[//' | sed 's/\]//' | sed 's/\,//'`; do
+     echo $x >> $tempfile
+   done
+   rm $tempfile.b
+  else
+   sidpath=$LiCSAR_procdir/subsets/$sid
+   tempfile=$BATCH_CACHE_DIR/$sid.frames
+   for sidf in `ls $sidpath/*/corners_clip.*`; do
+     echo $sidf | rev | cut -d '.' -f 1 | rev >> $tempfile
+   done
+  fi
+  #for x in `grep '\[' $tempfile | sed "s/'//g" | sed 's/\[//' | sed 's/\]//' | sed 's/\,//'`; do
+  for x in `cat $tempfile`; do
      echo "running background process for frame "$frame
      nohup framebatch_update_frame.sh -u $extra $x upfill > $tempfile.log.$x &
      sleep 15
@@ -85,7 +104,9 @@ fi
 
 # 2024/11: we want to clip to the same area...
 if [ $clipasportal == 0 ]; then
-  cliparea=`python3 -c "import volcdb; print(volcdb.get_licsbas_clipstring_volclip("$vid"))" | tail -n 1`
+  if [ -z $sid ]; then
+   cliparea=`python3 -c "import volcdb; print(volcdb.get_licsbas_clipstring_volclip("$vid"))" | tail -n 1`
+  fi
 else
   if [ -z $volcid ]; then
     volcid=`python3 -c "import volcdb; vid="$vid"; volcid=volcdb.get_volcano_from_vid(vid); print(volcid)"`
@@ -107,7 +128,12 @@ exit
 fi
 
 #volcid=$1
-vidpath=$LiCSAR_procdir/subsets/volc/$vid
+if [ ! -z $sid ]; then
+  vidpath=$LiCSAR_procdir/subsets/$sid
+else
+  vidpath=$LiCSAR_procdir/subsets/volc/$vid
+fi
+
 for subfr in `ls $vidpath`; do
   procpath=$BATCH_CACHE_DIR/subsets/$vid/$subfr
   # prepare licsbas script
@@ -119,16 +145,21 @@ for subfr in `ls $vidpath`; do
   #echo "licsar2licsbas.sh -M 3 -F -g -u -W -T -d -n 4 " >> $procpath/l2l.sh
   # 2024/01/31 - NOPE! ADF2 is horrible! using smooth, and from unfiltered - best results over Fogo! (or cascade, but that takes too long)
   #echo "licsar2licsbas.sh -M 3 -s -g -u -W -T -d -n 4 "$extra >> $procpath/l2l.sh
-  echo "licsar2licsbas.sh -M "$ml" -g -G "$cliparea" -u -d -T -t 0.2 -h 23 -n 4 "$lbextra >> $procpath/l2l.sh
+  if [ ! -z $cliparea ]; then
+   echo "licsar2licsbas.sh -M "$ml" -g -G "$cliparea" -u -d -T -t 0.2 -h 23 -n 4 "$lbextra >> $procpath/l2l.sh
+  else
+    echo "licsar2licsbas.sh -M "$ml" -g -u -d -T -t 0.2 -h 23 -n 4 "$lbextra >> $procpath/l2l.sh
+  fi
   chmod 777 $procpath/l2l.sh
   subset_mk_ifgs.sh $extra -s $procpath/l2l.sh -N $vidpath/$subfr
   # subset_mk_ifgs.sh $extra $vidpath/$subfr
 done
 
-
-volcid=`python3 -c "import volcdb; print(volcdb.get_volcano_from_vid("$vid"))" 2>/dev/null | tail -n 1`
-mkdir -p $BATCH_CACHE_DIR/subsets/per_volcano/$volcid
-cd $BATCH_CACHE_DIR/subsets/per_volcano/$volcid
-for subfr in `ls $vidpath`; do
-  ln -s $BATCH_CACHE_DIR/subsets/$vid/$subfr;
-done
+if [ -z $sid ]; then
+  volcid=`python3 -c "import volcdb; print(volcdb.get_volcano_from_vid("$vid"))" 2>/dev/null | tail -n 1`
+  mkdir -p $BATCH_CACHE_DIR/subsets/per_volcano/$volcid
+  cd $BATCH_CACHE_DIR/subsets/per_volcano/$volcid
+  for subfr in `ls $vidpath`; do
+    ln -s $BATCH_CACHE_DIR/subsets/$vid/$subfr;
+  done
+fi
