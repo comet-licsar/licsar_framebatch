48c30125b1058dde46b6aa2ddf8775e8734bf51f
espiritocz <espiritocz@gmail.com>
2026-03-13 01:50:34 +0000

    python env update: fixes

--- bin/licsar_make_frame.sh
+++ bin/licsar_make_frame.sh
@@ -351,14 +351,15 @@ cat << EOF > getit.py
 import pandas as pd
 from batchDBLib import engine
 from configLib import config
-from sqlalchemy import text
+# from sqlalchemy import text
 sqlPath = config.get('Config','SQLPath')
 QryFile = open(sqlPath+'/$stepsql.sql','r')
 if QryFile:
     Qry = QryFile.read()
     Qry = Qry.split('WHERE')[0]+'WHERE polygs.polyid_name = "$frame" and jobs.user = "$USER" and polygs.active = TRUE;'
     with engine.connect() as conn:
-        DatFrm = pd.read_sql_query(text(Qry),conn)
+        result = conn.execute(Qry)
+    DatFrm = pd.DataFrame(result.fetchall())
     #
     # DatFrm = DatFrm.query('Frame == "$frame" and User == "$USER"')
     try:
