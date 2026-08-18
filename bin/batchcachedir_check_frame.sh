71b9c6c6bca5d06ac0e94b10dfbf525690af8a35
espiritocz <espiritocz@gmail.com>
2026-08-18 13:03:06 +0100

    bcf: disabling postproc coreg of slcs with missing bursts

--- bin/batchcachedir_check_frame.sh
+++ bin/batchcachedir_check_frame.sh
@@ -218,12 +218,13 @@ done
      if [ `ls SLC.missingbursts 2>/dev/null | wc -l` -gt 0 ]; then
        echo "there are files in SLC.missingbursts:"
        ls SLC.missingbursts
-       if [ $PROC == 1 ]; then
-         echo "we will force coregister them now"
-         mv SLC.missingbursts/* SLC/.
-         framebatch_postproc_coreg.sh -F $frame 1
-         exit
-       fi
+       echo "we will not attempt for their coregistration - you can do this manually by:"
+       #if [ $PROC == 1 ]; then
+       #  echo "we will force coregister them now"
+       echo "mv SLC.missingbursts/* SLC/."
+       echo "framebatch_postproc_coreg.sh -F "$frame" 1"
+       #  exit
+       #fi
      fi
     if [ $rslcdates -lt 2 ]; then todel=1;
     else
